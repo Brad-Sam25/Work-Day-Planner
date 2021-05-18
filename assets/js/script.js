024456034b10da55ff42ffb439ec8de74cf94d9e
Brad Samuel <bradsamuel97@gmail.com>
2021-05-17 21:10:12 -0400

    saving final changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,114 +1,172 @@
 let textAreaEl = $("#text-area"); 
-let nine = $("#900");
-let ten = $("#1000");
-let eleven = $("#1100");
-let twelve = $("#1200");
-let thirteen = $("#1300");
-let fourteen = $("#1400");
-let fifteen = $("#1500");
-let sixteen = $("#1600");
-let seventeen = $("#1700");  
-let rightNow = moment().format('HH:mm a');
+let nine = $("#text-area9");
+let ten = $("#text-area10");
+let eleven = $("#text-area11");
+let twelve = $("#text-area12");
+let thirteen = $("#text-area13");
+let fourteen = $("#text-area14");
+let fifteen = $("#text-area15");
+let sixteen = $("#text-area16");
+let seventeen = $("#text-area17");  
+let rightNow = moment().format('H');
+
+$(document).ready(function() {
+    let ls9 = localStorage.getItem("nine")
+    let ls10 = localStorage.getItem("ten")
+    let ls11 = localStorage.getItem("eleven")
+    let ls12 = localStorage.getItem("twelve")
+    let ls13= localStorage.getItem("thirteen")
+    let ls14 = localStorage.getItem("fourteen")
+    let ls15 = localStorage.getItem("fifteen")
+    let ls16 = localStorage.getItem("sixteen")
+    let ls17 = localStorage.getItem("seventeen")
+
+    nine.text(ls9);
+    ten.text(ls10);
+    eleven.text(ls11);
+    twelve.text(ls12);
+    thirteen.text(ls13);
+    fourteen.text(ls14);
+    fifteen.text(ls15);
+    sixteen.text(ls16);
+    seventeen.text(ls17);
+})
+
 
 function showBlockColor () {
-    parseInt($("#900").val(), rightNow)
-    if (nine < rightNow) {
-        $(textAreaEl).addClass("past")
-    } else if (nine > rightNow) {
-        $(textAreaEl).removeClass("past").addClass("future")
+console.log(rightNow)
+    if (9 < rightNow) {
+        nine.removeClass("present").removeClass("future").addClass("past")
+    } else if (9 > rightNow) {
+        nine.removeClass("past").removeClass("present").addClass("future")
     } else {
-        $(textAreaEl).removeClass("future").addClass("present")
+        nine.removeClass("future").removeClass("past").addClass("present")
     }
 
-    parseInt($("#1000").val(), rightNow)
-    if (ten < rightNow) {
-        $(textAreaEl).addClass("past")
-    } else if (ten > rightNow) {
-        $(textAreaEl).removeClass("past").addClass("future")
+    if (10 < rightNow) {
+        ten.removeClass("present").removeClass("future").addClass("past")
+    } else if (10 > rightNow) {
+        ten.removeClass("past").removeClass("present").addClass("future")
     } else {
-        $(textAreaEl).removeClass("future").addClass("present")
+        ten.removeClass("future").removeClass("past").addClass("present")
     }
 
-    parseInt($("#1100").val(), rightNow)
-    if (eleven < rightNow) {
-        $(textAreaEl).addClass("past")
-    } else if (eleven > rightNow) {
-        $(textAreaEl).removeClass("past").addClass("future")
+    if (11 < rightNow) {
+        eleven.removeClass("present").removeClass("future").addClass("past")
+    } else if (11 > rightNow) {
+        eleven.removeClass("past").removeClass("present").addClass("future")
     } else {
-        $(textAreaEl).removeClass("future").addClass("present")
+        eleven.removeClass("future").removeClass("past").addClass("present")
     }
 
-    parseInt($("#1200").val(), rightNow)
-    if (twelve < rightNow) {
-        $(textAreaEl).addClass("past")
-    } else if (twelve > rightNow) {
-        $(textAreaEl).removeClass("past").addClass("future")
+    if (12 < rightNow) {
+        twelve.removeClass("present").removeClass("future").addClass("past")
+    } else if (12 > rightNow) {
+        twelve.removeClass("past").removeClass("present").addClass("future")
     } else {
-        $(textAreaEl).removeClass("future").addClass("present")
+        twelve.removeClass("future").removeClass("past").addClass("present")
     }
 
-    parseInt($("#1300").val(), rightNow)
-    if (thirteen < rightNow) {
-        $(textAreaEl).addClass("past")
-    } else if (thirteen > rightNow) {
-        $(textAreaEl).removeClass("past").addClass("future")
+    if (13 < rightNow) {
+        thirteen.removeClass("present").removeClass("future").addClass("past")
+    } else if (13 > rightNow) {
+        thirteen.removeClass("past").removeClass("present").addClass("future")
     } else {
-        $(textAreaEl).removeClass("future").addClass("present")
+        thirteen.removeClass("future").removeClass("past").addClass("present")
     }
 
-    parseInt($("#1400").val(), rightNow)
-    if (fourteen < rightNow) {
-        $(textAreaEl).addClass("past")
-    } else if (fourteen > rightNow) {
-        $(textAreaEl).removeClass("past").addClass("future")
+    if (14 < rightNow) {
+        fourteen.removeClass("present").removeClass("future").addClass("past")
+    } else if (14 > rightNow) {
+        fourteen.removeClass("past").removeClass("present").addClass("future")
     } else {
-        $(textAreaEl).removeClass("future").addClass("present")
+        fourteen.removeClass("future").removeClass("past").addClass("present")
     }
     
-    parseInt($("#1500").val(), rightNow)
-    if (fifteen < rightNow) {
-        $(textAreaEl).addClass("past")
-    } else if (fifteen > rightNow) {
-        $(textAreaEl).removeClass("past").addClass("future")
+    if (15 < rightNow) {
+        fifteen.removeClass("present").removeClass("future").addClass("past")
+    } else if (15 > rightNow) {
+        fifteen.removeClass("past").removeClass("present").addClass("future")
     } else {
-        $(textAreaEl).removeClass("future").addClass("present")
+        fifteen.removeClass("future").removeClass("past").addClass("present")
     }
     
-    parseInt($("#1600").val(), rightNow)
-    if (sixteen < rightNow) {
-        $(textAreaEl).addClass("past")
-    } else if (sixteen > rightNow) {
-        $(textAreaEl).removeClass("past").addClass("future")
+    if (16 < rightNow) {
+        sixteen.removeClass("present").removeClass("future").addClass("past")
+    } else if (16 > rightNow) {
+        sixteen.removeClass("past").removeClass("present").addClass("future")
     } else {
-        $(textAreaEl).removeClass("future").addClass("present")
+        sixteen.removeClass("future").removeClass("past").addClass("present")
     }
     
-    parseInt($("#1700").val(), rightNow)
-    if (seventeen < rightNow) {
-        $(textAreaEl).addClass("past")
-    } else if (seventeen > rightNow) {
-        $(textAreaEl).removeClass("past").addClass("future")
+    if (17 < rightNow) {
+        seventeen.removeClass("present").removeClass("future").addClass("past")
+    } else if (17 > rightNow) {
+        seventeen.removeClass("past").removeClass("present").addClass("future")
     } else {
-        $(textAreaEl).removeClass("future").addClass("present")
+        seventeen.removeClass("future").removeClass("past").addClass("present")
     }
 }
 
 showBlockColor();
 
-console.log(nine);
 
-$("#saves").on("click", function () {
-    localStorage.setItem('event', JSON.stringify(textAreaEl));
+
+$("#saves9").on("click", function () {
+    let input = nine.val();
+    localStorage.setItem("nine", input);
 })
 
-function getSavedText (event) {
-    if (!localStorage.getItem(textAreaEl)) {
-        return '';
-    }
-    return localStorage.getItem(textAreaEl)
-}
+$("#saves10").on("click", function () {
+    let input = ten.val();
+    localStorage.setItem("ten", input);
+})
 
-getSavedText();
+$("#saves11").on("click", function () {
+    let input = eleven.val();
+    localStorage.setItem("eleven", input);
+})
+
+$("#saves12").on("click", function () {
+    let input = twelve.val();
+    localStorage.setItem("twelve", input);
+})
+
+$("#saves13").on("click", function () {
+    let input = thirteen.val();
+    localStorage.setItem("thirteen", input);
+})
+
+$("#saves14").on("click", function () {
+    let input = fourteen.val();
+    localStorage.setItem("fourteen", input);
+})
+
+$("#saves15").on("click", function () {
+    let input = fifteen.val();
+    localStorage.setItem("fifteen", input);
+})
+
+$("#saves16").on("click", function () {
+    let input = sixteen.val();
+    localStorage.setItem("sixteen", input);
+})
+
+$("#saves17").on("click", function () {
+    let input = seventeen.val();
+    localStorage.setItem("seventeen", input);
+})
+
+
+
+// function getSavedText (event) {
+//     if (!localStorage.getItem(textAreaEl)) {
+//         return '';
+//     }
+//     return localStorage.getItem(textAreaEl)
+// }
+
+// getSavedText();
 
 function showTime () {
     let currentTime = moment().format('MMMM DD, YYYY [at] hh:mm:ss a');
